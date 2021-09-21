99df98804ac8dbafc9be4ee516347c4094819e50
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-21 09:04:20 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+#run function
+print(topFiveMovies("Phineas and Ferb ", "Spongebob ", "Psych ", "Murder, She Wrote ", "Numb3rs"))
